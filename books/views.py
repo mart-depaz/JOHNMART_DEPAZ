963b69b101c18b6c3909ccb6c63ed45403466b74
johnmart <johnmartdepaz77@gmail.com>
2025-02-11 19:56:33 +0800

    Updated <filename> with new content

--- books/views.py
+++ books/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+from .forms import BookForm
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+
+    return render(request, 'books/book_list.html', {'bools': books})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})
+
+def book_update(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book/book_form.html', {'form': form})
+
+def book_delete(request, pk):
+    book = Book.objects.get(pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/book_confirm_delete.html', {'book': book})
